e7e900130ef23f43bb49d3b0a123d3b8eeebda5d
Jason Pham <s4354618@gmail.com>
2017-07-03 23:03:30 +1000

    Updated serial input - Handles unique start/end identifiers. Used with Electron App!

--- arduinoLCD/src/dnd_lcd.cpp
+++ arduinoLCD/src/dnd_lcd.cpp
@@ -5,30 +5,76 @@
 #include <LiquidCrystal_I2C.h>
 #include <StandardCplusplus.h>
 
+/** Prototype function declarations **/
+void recvWithStartEndMarkers();
+void setMux(int x);
+
 /** Definitions and global variables **/
 #define CONTROL0 5
 #define CONTROL1 4
 #define CONTROL2 3
 #define CONTROL3 2
 
+//Array to manage MUX chip
 int mux0array[16];
 int mux1array[16];
 int mux2array[16];
 
+//LCD code management
 int numberOfLCD = 10;
 LiquidCrystal_I2C lcd(0x38,20,4);
+
+//List storing who to send MSG to
 LinkedList<int> myLinkedList = LinkedList<int>();
+
+//Serial input
+const byte numChars = 250;
+char receivedChars[numChars];   // an array to store the received data
+boolean newData = false;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("<Arduino is ready>");
 }
 
 void loop() {
-  double math = ceil(2.3);
-  Serial.println(math);
+  recvWithStartEndMarkers();
+  if (newData == true) {
+    Serial.println(receivedChars);
+    newData = false;
+  }
+}
 
-  Serial.println("Hello World");
-  delay(5000);
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
 }
 
 void setMux(int x) {
