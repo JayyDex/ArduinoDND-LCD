5c19fd8afa3c583d78ccfde7751229237a90cc03
Jason Pham <s4354618@gmail.com>
2017-07-04 01:16:39 +1000

    String split (Msg part done). Recipient Side, add to linkedList, conduct minor digit checking

--- arduinoLCD/src/dnd_lcd.cpp
+++ arduinoLCD/src/dnd_lcd.cpp
@@ -4,10 +4,25 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <StandardCplusplus.h>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <cctype>
+#include <locale>
+#include <sstream>
+#include <iostream>
 
 /** Prototype function declarations **/
 void recvWithStartEndMarkers();
 void setMux(int x);
+const char* c_str();
+void routeDecider(int sequence);
+void breakMessage(const char* rawMsg);
+static inline std::string &trim(std::string &s);
+void split(const std::string& s, char seperator);
+bool is_number(const std::string& s);
 
 /** Definitions and global variables **/
 #define CONTROL0 5
@@ -25,12 +40,17 @@ int numberOfLCD = 10;
 LiquidCrystal_I2C lcd(0x38,20,4);
 
 //List storing who to send MSG to
-LinkedList<int> myLinkedList = LinkedList<int>();
+LinkedList<int> multicastList = LinkedList<int>();
 
 //Serial input
 const byte numChars = 250;
 char receivedChars[numChars];   // an array to store the received data
+std::string backUp;
 boolean newData = false;
+
+//Msg parts of input
+std::string receiverPart;
+std::string msgPart;
 
 void setup() {
   Serial.begin(9600);
@@ -40,41 +60,81 @@ void setup() {
 void loop() {
   recvWithStartEndMarkers();
   if (newData == true) {
-    Serial.println(receivedChars);
+    int sequence = 0;
+
+    if (receivedChars[0] == 'r') {
+      if (backUp.empty() == 1) {
+        //Means there is no Msg to Repeat
+        Serial.println("ERROR: There is no repeatable msg");
+        sequence = -1;
+      } else {
+        sequence = 1;
+        breakMessage(backUp.c_str());
+      }
+    } else {
+      breakMessage(receivedChars);
+    }
+
+    routeDecider(sequence);
+
+    multicastList.clear();
     newData = false;
   }
 }
 
-void recvWithStartEndMarkers() {
-    static boolean recvInProgress = false;
-    static byte ndx = 0;
-    char startMarker = '<';
-    char endMarker = '>';
-    char rc;
+void routeDecider(int sequence) {
+  switch(sequence) {
+    case 0:
+      //Normal Route
+      Serial.println("Normal Route");
+      backUp = receivedChars;
 
-    while (Serial.available() > 0 && newData == false) {
-        rc = Serial.read();
+      Serial.println("Inside List");
+      for (int g = 0; g < multicastList.size(); g++) {
+        Serial.print(multicastList.get(g));
+      }
+      Serial.println("");
+      Serial.println(receiverPart.c_str());
+      Serial.println(msgPart.c_str());
+      break;
+    case 1:
+      //Repeat Previous Msg
+      Serial.println("Repeat Route");
 
-        if (recvInProgress == true) {
-            if (rc != endMarker) {
-                receivedChars[ndx] = rc;
-                ndx++;
-                if (ndx >= numChars) {
-                    ndx = numChars - 1;
-                }
-            }
-            else {
-                receivedChars[ndx] = '\0'; // terminate the string
-                recvInProgress = false;
-                ndx = 0;
-                newData = true;
-            }
-        }
+      Serial.println("Inside List");
+      for (int g = 0; g < multicastList.size(); g++) {
+        Serial.println(multicastList.get(g));
+      }
+      Serial.println("");
+      Serial.print(receiverPart.c_str());
+      Serial.println(msgPart.c_str());
+      break;
+    default:
+      Serial.println("Invalid Route");
+      break;
+  }
+}
 
-        else if (rc == startMarker) {
-            recvInProgress = true;
-        }
+void messageController() {
+
+}
+
+void breakMessage(const char* rawMsg) {
+  std::string CppString = rawMsg;
+  std::size_t pos = CppString.find("-");
+
+  receiverPart = CppString.substr(0,pos);
+  msgPart = CppString.substr(pos+1);
+  trim(receiverPart);
+  trim(msgPart);
+
+  if(receiverPart.c_str()[0] == 'a') {
+    for(int i = 0; i < numberOfLCD; i++) {
+      multicastList.add(i);
     }
+  } else {
+    split(receiverPart, ',');
+  }
 }
 
 void setMux(int x) {
@@ -82,4 +142,83 @@ void setMux(int x) {
   digitalWrite(CONTROL1, (x&7)>>2);
   digitalWrite(CONTROL2, (x&3)>>1);
   digitalWrite(CONTROL3, (x&1));
+}
+
+/**
+Other people code below, mainly helper functions
+**/
+
+//Handle serial input, using markers "<>"
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+}
+
+// trim from start
+static inline std::string &ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s) {
+    return ltrim(rtrim(s));
+}
+
+//Tokenise string, and add to multi-cast list (Modified)
+void split(const std::string& s, char seperator) {
+    std::string::size_type prev_pos = 0, pos = 0;
+    while((pos = s.find(seperator, pos)) != std::string::npos)
+    {
+        std::string substring( s.substr(prev_pos, pos-prev_pos));
+        if (is_number(substring)) {
+          int value = std::strtol(substring.c_str(), NULL, 10);
+          multicastList.add(value);
+        }
+        prev_pos = ++pos;
+    }
+    std::string lastSubString( s.substr(prev_pos, pos-prev_pos));
+    if (is_number(lastSubString)) {
+      int lastValue = std::strtol(lastSubString.c_str(), NULL, 10);
+      multicastList.add(lastValue);
+    }
+}
+
+bool is_number(const std::string& s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
 }
